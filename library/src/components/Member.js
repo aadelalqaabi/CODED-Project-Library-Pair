e5cd13ac49517d2f1ef77af1fe8f06126e014e49
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-05-19 20:35:33 +0300

    Finished all members and on final step of books

--- library/src/components/Member.js
+++ library/src/components/Member.js
@@ -1,15 +1,33 @@
 import bookStore from "../stores/bookStore";
+import { Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
 function Member({ member }) {
   const bookTitles = member.currentlyBorrowedBooks.map((borrowedBook) => {
     return bookStore.findBookTitle(borrowedBook.id);
   });
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="onemember">
       <h2>
         {member.firstName} {member.lastName}
       </h2>
       <h2>{member.membership}</h2>
-      <p>{bookTitles}</p>
+      <div className="mx-2">
+        <Button variant="light" onClick={handleShow}>
+          Show Details
+        </Button>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {member.firstName} {member.lastName}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{bookTitles}</Modal.Body>
+      </Modal>
     </div>
   );
 }
